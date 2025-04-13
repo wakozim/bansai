a91b8715b14c4393047284e9ce502d8d603ba5e3
Lookins01 <lookins1q@gmail.com>
2025-04-13 17:33:37 +0500

    [feat] add buttons for control board size and add/remove banner

--- script.js
+++ script.js
@@ -35,8 +35,12 @@ class Pattern {
     }
 }
 
-const PatternType = createEnum(['roundel', 'baseDexterCanton', 'baseSinisterCanton', 'chiefDexterCanton', 'chiefSinisterCanton', 'perFess', ]);
-const PatternColor = createEnum(['white', 'orange', 'purple', 'lightBlue', 'yellow']);
+const PatternType = createEnum(
+    ['roundel', 'baseDexterCanton', 'baseSinisterCanton', 'chiefDexterCanton', 'chiefSinisterCanton', 'perFess', ]
+);
+const PatternColor = createEnum(
+    ['white', 'orange', 'purple', 'lightBlue', 'yellow']
+);
 
 
 let z_index = 1000;
@@ -65,17 +69,19 @@ class Banner {
         this.patterns[index].color = color;
     }
 
-    asHtml() {
+    asHtml(x, y) {
+        const delay = x*0.05 + y*0.07;
+
         const banner = Object.assign(document.createElement('div'), {
             'className': 'banner',
-            'style': `background-color: ${this.color}; z-index: ${z_index}`,
+            'style': `background-color: ${this.color}; z-index: ${z_index}; animation-delay: ${delay}s`,
         });
-        
+
         let parentElement = banner;
         for (let i = 0; i < this.patterns.length; ++i) {
-            let temp = this.patterns[i].asHtml();
-            parentElement.appendChild(temp);
-            parentElement = temp;
+            let pattern = this.patterns[i].asHtml();
+            parentElement.appendChild(pattern);
+            parentElement = pattern;
         }
 
         const banner_shadow = Object.assign(document.createElement('div'), {
@@ -86,8 +92,48 @@ class Banner {
     }
 }
 
+class Block {
+    constructor(x, y, id) {
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.banner = null;
+        this.selected = false;
+    }
+
+    addBanner(color = undefined) {
+       this.banner = new Banner(color); 
+    }
+
+    asHtml() {
+        const blocksColumn = Object.assign(document.createElement('div'), {
+            'className': 'blocks-column',
+            'id': this.id,
+        });
+        if (this.selected) blocksColumn.classList.toggle('selected'); 
+
+        const stone = Object.assign(document.createElement('img'), {
+            'src': './stone.png',
+            'className': 'block',
+        });
+        blocksColumn.appendChild(stone);
+
+        if (this.banner !== null) blocksColumn.appendChild(this.banner.asHtml(this.x, this.y));
+        return blocksColumn;
+    }
+
+    update() {
+        console.log('test');
+        const oldBlocksColumn = document.getElementById(this.id);
+        const parentDiv = document.getElementById(this.id).parentElement;
+        parentDiv.replaceChild(this.asHtml(), oldBlocksColumn);
+    }
+}
+
+
 class Board {
     #reset() {
+        this.id = 0;
         this.z_index = 0;
         this.rows = 0;
         this.cols = 0;
@@ -98,48 +144,159 @@ class Board {
     constructor(rows, cols) {
         this.#reset();
 
-        this.rows = 3;
-        this.cols = 3;
-        this.blocks = [];
         this.selectedBlock = 0;
+        this.rows = rows;
+        this.cols = cols;
+        this.blocks = [];
+        this.selectedBlock = null;
+
+        for (let y = 0; y < this.rows; ++y) {
+            const row = new Array();
+            for (let x = 0; x < this.cols; ++x) {
+                row.push(new Block(x, y, this.id++));
+            }
+            this.blocks.push(row);
+        }
+    }
+
+    addRow() {
+        const row = new Array(this.cols);
+        for (let x = 0; x < this.cols; ++x) {
+            row[x] = new Block(x, this.rows, this.id++);
+        }
+        this.rows += 1;
+        this.blocks.push(row);
+
+        this.update();
+    }
+
+    removeRow() {
+        if (this.rows <= 1) return;
+
+        this.rows -= 1;
+        this.blocks.pop();
+
+        this.update();
+    }
+
+    addColumn() {
+        for (let y = 0; y < this.rows; ++y) {
+            this.blocks[y].push(new Block(this.cols, y, this.id++));
+        }
+        this.cols += 1;
+        
+        this.update();
+    }
+
+    removeColumn() {
+        if (this.cols <= 1) return;
+
+        this.cols -= 1;
+        for (let y = 0; y < this.rows; ++y) {
+            this.blocks[y].pop();
+        }
+
+        this.update();
+    }
+
+    update() {
+        this.blocksDiv.replaceChildren();
+        this.asHtml().forEach((x) => {this.blocksDiv.appendChild(x)});
+    }
+
+    asHtml() {
+        const board = [];
+        for (let y = 0; y < this.rows; ++y) {
+            let blocksRow = Object.assign(document.createElement('div'), {
+                'className': 'blocks-row',
+            });
+            for (let x = 0; x < this.cols; ++x) {
+                const block = this.blocks[y][x];
+                block.selected = this.selectedBlock === block; 
+                let blocksColumn = block.asHtml();
+                blocksColumn.addEventListener('click', (e) => {
+                    e.preventDefault();
+                    
+                    this.selectedBlock = block;
+                    this.update();
+                    
+                    if (block.selected === false) return;
+
+                    document.getElementById("add-banner").onclick = () => {
+                        if (block.banner !== null) return;
+                        block.banner = new Banner();
+                        block.update();
+                    };
+                    document.getElementById("remove-banner").onclick = () => {
+                        block.banner = null;
+                        block.update();
+                    };
+
+                });
+                blocksRow.appendChild(blocksColumn);
+            }
+            board.push(blocksRow);
+        }
+        return board;
     }
 }
 
+const board = new Board(3, 3);
 
-document.addEventListener("DOMContentLoaded", () => { 
+document.addEventListener("DOMContentLoaded", () => {
     const blocks = document.getElementById('blocks');
-    
+    board.blocksDiv = blocks;
+
     if (!blocks) {
         throw new Error('Can\'t get element `blocks`. Try to reload the page!');
     }
 
-    blocks.replaceChildren();;
+    blocks.replaceChildren();
 
+    document.getElementById("add-row").addEventListener("click", () => {
+        board.addRow();
+    });
+
+    document.getElementById("remove-row").addEventListener("click", () => {
+        board.removeRow();
+    });
+
+    document.getElementById("add-column").addEventListener("click", () => {
+        board.addColumn();
+    });
     
+    document.getElementById("remove-column").addEventListener("click", () => {
+        board.removeColumn();
+    });
+
+    board.update();
+    return;
+
+
     let blocks_row = Object.assign(document.createElement('div'), {
         'className': 'blocks-row',
     });
-    
-    addBanner(blocks_row);
-    addBanner(blocks_row);
-    addBanner(blocks_row);
+
+    let i = 0;
+    addBanner(blocks_row, i++);
+    addBanner(blocks_row, i++);
+    addBanner(blocks_row, i++);
 
     blocks.appendChild(blocks_row);
     z_index--;
-    
+
     blocks_row = Object.assign(document.createElement('div'), {
         'className': 'blocks-row',
     });
-    
-    addBanner(blocks_row);
-    addBanner(blocks_row);
-    addBanner(blocks_row);
+
+    addBanner(blocks_row, i++);
+    addBanner(blocks_row, i++);
+    addBanner(blocks_row, i++);
 
     blocks.appendChild(blocks_row);
 
-
     return;
-    
+
 
     blocks.appendChild(blocks_row);
     z_index--;
@@ -154,12 +311,12 @@ document.addEventListener("DOMContentLoaded", () => {
     blocks.appendChild(blocks_row);
 });
 
-function addBanner(blocks_row) {
-    const banner = new Banner('rgb(255, 0, 0)');
-    //banner.addPattern(PatternType.baseSinisterCanton, PatternColor.white);
+function addBanner(blocks_row, i) {
+    const banner = new Banner('#7a201b');
+    banner.addPattern(PatternType.baseSinisterCanton, PatternColor.white);
     //banner.addPattern(PatternType.baseDexterCanton, PatternColor.white);
-    //banner.addPattern(PatternType.roundel, PatternColor.yellow);
-    
+    banner.addPattern(PatternType.roundel, PatternColor.yellow);
+
     const blocks_column = Object.assign(document.createElement('div'), {
         'className': 'blocks-column',
     });
@@ -171,6 +328,5 @@ function addBanner(blocks_row) {
     });
     blocks_column.appendChild(stone);
 
-    
-    blocks_column.appendChild(banner.asHtml());    
+    blocks_column.appendChild(banner.asHtml(i/3, i%3));
 }
